150867f4d5642e464aed843154fe7b29f029ddde
Tako-San <dxe13@mail.ru>
2022-02-16 22:34:05 +0300

    Added Plane class

--- include/primitives/plane.hh
+++ include/primitives/plane.hh
@@ -1,10 +1,118 @@
 #ifndef __INCLUDE_PRIMITIVES_PLANE_HH__
 #define __INCLUDE_PRIMITIVES_PLANE_HH__
 
+#include <iostream>
+
+#include "line.hh"
+#include "vector.hh"
+
 namespace geom
 {
 
-} // namespace geom
+template <std::floating_point T>
+class Plane final
+{
+private:
+  Vector<T> norm_{}; // normal vector, length equals to 1
+  T dist_{};         // distance from zero to plane
 
+  Plane(const Vector<T> &norm, T dist);
+
+public:
+  T dist() const;
+  const Vector<T> &norm() const;
+
+  bool belongs(const Vector<T> &point) const;
+  bool belongs(const Line<T> &line) const;
+  bool isEqual(const Plane &rhs) const;
+
+  static Plane getBy3Points(const Vector<T> &p1, const Vector<T> &p2,
+                            const Vector<T> &p3);
+  static Plane getParametric(const Vector<T> &org, const Vector<T> &dir1,
+                             const Vector<T> &dir2);
+  static Plane getNormalPoint(const Vector<T> &norm, const Vector<T> &point);
+  static Plane getNormalDist(const Vector<T> &norm, T constant);
+};
+
+template <std::floating_point T>
+Plane<T>::Plane(const Vector<T> &norm, T dist) : norm_(norm), dist_(dist)
+{
+  if (norm == Vector<T>{0})
+    throw std::logic_error{"normal vector equals to zero"};
+}
+
+template <std::floating_point T>
+T Plane<T>::dist() const
+{
+  return dist_;
+}
+
+template <std::floating_point T>
+const Vector<T> &Plane<T>::norm() const
+{
+  return norm_;
+}
+
+template <std::floating_point T>
+bool Plane<T>::belongs(const Vector<T> &point) const
+{
+  return Vector<T>::isNumEq(norm_.dot(point), dist_);
+}
+
+template <std::floating_point T>
+bool Plane<T>::belongs(const Line<T> &line) const
+{
+  return norm_.isPerp(line.dir()) && belongs(line.org());
+}
+
+template <std::floating_point T>
+bool Plane<T>::isEqual(const Plane &rhs) const
+{
+  return (norm_ * dist_ == rhs.norm_ * rhs.dist_) && (norm_.isPar(rhs.norm_));
+}
+
+template <std::floating_point T>
+Plane<T> Plane<T>::getBy3Points(const Vector<T> &p1, const Vector<T> &p2,
+                                const Vector<T> &p3)
+{
+  return getParametric(p1, p2 - p1, p3 - p1);
+}
+
+template <std::floating_point T>
+Plane<T> Plane<T>::getParametric(const Vector<T> &org, const Vector<T> &dir1,
+                                 const Vector<T> &dir2)
+{
+  auto norm = dir1.cross(dir2);
+  return getNormalPoint(norm, org);
+}
+
+template <std::floating_point T>
+Plane<T> Plane<T>::getNormalPoint(const Vector<T> &norm, const Vector<T> &point)
+{
+  auto normalized = norm.normalized();
+  return Plane{normalized, normalized.dot(point)};
+}
+
+template <std::floating_point T>
+Plane<T> Plane<T>::getNormalDist(const Vector<T> &norm, T dist)
+{
+  auto normalized = norm.normalized();
+  return Plane{normalized, dist};
+}
+
+template <std::floating_point T>
+bool operator==(const Plane<T> &pl, const Plane<T> &pr)
+{
+  return pl.isEqual(pr);
+}
+
+template <std::floating_point T>
+std::ostream &operator<<(std::ostream &ost, const Plane<T> &p)
+{
+  ost << p.norm() << " * X = " << p.dist();
+  return ost;
+}
+
+} // namespace geom
 
 #endif // __INCLUDE_PRIMITIVES_PLANE_HH__
--- test/plane_tst/main.cc
+++ test/plane_tst/main.cc
@@ -1,6 +1,102 @@
+#include <concepts>
+#include <sstream>
+
 #include <gtest/gtest.h>
 
-#include "primitives/line.hh"
+#include "primitives/plane.hh"
+
+using namespace geom;
+
+template <std::floating_point T>
+static void checkCtor(const Plane<T> &p, const Vector<T> &normExpected, T distExpected)
+{
+  // Act
+  auto norm = p.norm();
+  auto dist = p.dist();
+
+  // Assert
+  ASSERT_TRUE(normExpected.isEqual(norm));
+  ASSERT_TRUE(Vector<T>::isNumEq(distExpected, dist));
+}
+
+TEST(Plane, ctor)
+{
+  // Arrange
+  auto p1 = Plane<double>::getNormalDist({7, 0, 0}, 4);
+  auto p2 = Plane<double>::getParametric({0, 0, 1}, {1, 0, 0}, {0, 1, 0});
+  auto p3 = Plane<double>::getNormalPoint({0, 0, 1}, {1, 1, 1});
+  auto p4 = Plane<double>::getBy3Points({0, 0, 1}, {1, 1, 1}, {5, 2, 1});
+
+  // Act & Assert
+  checkCtor<double>(p1, {1, 0, 0}, 4);
+  checkCtor<double>(p2, {0, 0, 1}, 1);
+  checkCtor<double>(p3, {0, 0, 1}, 1);
+  checkCtor<double>(p4, {0, 0, -1}, -1);
+}
+
+TEST(Plane, belongsPoint)
+{
+  // Arrange
+  auto p1 = Plane<double>::getParametric({1, 0, 0}, {-1, 0, 1}, {-1, -1, 0});
+  auto p2 = Plane<double>::getNormalPoint({-1, -1, -1}, {0, 0, 0});
+
+  // Act
+  auto resTrue1 = p1.belongs(VectorD{0, 0, 1});
+  auto resFalse1 = p1.belongs(VectorD{0, 0, 2});
+
+  auto resTrue2 = p2.belongs(VectorD{1, 1, -2});
+  auto resFalse2 = p2.belongs(VectorD{1, 1, 1});
+
+  // Assert
+  ASSERT_TRUE(resTrue1);
+  ASSERT_FALSE(resFalse1);
+
+  ASSERT_TRUE(resTrue2);
+  ASSERT_FALSE(resFalse2);
+}
+
+TEST(Plane, belongsLine)
+{
+  // Arrange
+  auto p = Plane<double>::getParametric({1, 1, 1}, {1, 2, 1}, {2, 1, 1});
+  Line<double> l1{{1, 1, 1}, {1, 2, 1}};
+  Line<double> l2{{2, 2, 2}, {1, 2, 1}};
+
+  // Act & Assert
+  ASSERT_TRUE(p.belongs(l1));
+  ASSERT_FALSE(p.belongs(l2));
+}
+
+TEST(Plane, isEqual)
+{
+  // Arrange
+  auto p1 = Plane<double>::getNormalDist({3, 4, 5}, 17);
+  auto p2 = Plane<double>::getNormalDist({-3, -4, -5}, -17);
+  auto p3 = Plane<double>::getNormalDist({3, 4, 5}, -17);
+  auto p4 = Plane<double>::getNormalDist({-3, -4, -5}, 17);
+
+  // Act & Assert
+  ASSERT_EQ(p1, p2);
+  ASSERT_EQ(p3, p4);
+
+  ASSERT_NE(p1, p3);
+  ASSERT_NE(p1, p4);
+  ASSERT_NE(p2, p3);
+  ASSERT_NE(p2, p4);
+}
+
+TEST(Plane, ostream)
+{
+  // Arrange
+  auto p = Plane<double>::getNormalDist({2, 0, 0}, 17);
+
+  // Act
+  std::stringstream ss{};
+  ss << p;
+
+  // Assert
+  ASSERT_EQ(ss.str(), "(1, 0, 0) * X = 17");
+}
 
 int main(int argc, char **argv)
 {
