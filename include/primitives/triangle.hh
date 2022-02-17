e7b0b6ff957f07a1a9046a1ec998a6289651de55
Tako-San <dxe13@mail.ru>
2022-02-17 23:02:16 +0300

    Added triangle class, ctor and operator[]

--- include/primitives/triangle.hh
+++ include/primitives/triangle.hh
@@ -1,4 +1,37 @@
 #ifndef __INCLUDE_PRIMITIVES_TRIANGLE_HH__
 #define __INCLUDE_PRIMITIVES_TRIANGLE_HH__
 
+#include <array>
+#include <concepts>
+
+#include "vector.hh"
+
+namespace geom
+{
+
+template <std::floating_point T>
+class Triangle final
+{
+private:
+  std::array<Vector<T>, 3> vertices_;
+
+public:
+  Triangle(const Vector<T> &p1, const Vector<T> &p2, const Vector<T> &p3);
+  const Vector<T> &operator[](std::size_t idx) const;
+};
+
+template <std::floating_point T>
+Triangle<T>::Triangle(const Vector<T> &p1, const Vector<T> &p2, const Vector<T> &p3)
+  : vertices_{p1, p2, p3}
+{
+}
+
+template <std::floating_point T>
+const Vector<T> &Triangle<T>::operator[](std::size_t idx) const
+{
+  return vertices_[idx % 3];
+}
+
+} // namespace geom
+
 #endif // __INCLUDE_PRIMITIVES_TRIANGLE_HH__
--- test/triangle_tst/main.cc
+++ test/triangle_tst/main.cc
@@ -1,5 +1,23 @@
 #include <gtest/gtest.h>
 
+#include "primitives/triangle.hh"
+
+using namespace geom;
+
+TEST(Triangle, ctor)
+{
+  // Arrange
+  VectorD v1{1, 2, 3};
+  VectorD v2{4, 5, 6};
+  VectorD v3{7, 8, 9};
+  Triangle tr{v1, v2, v3};
+
+  // Act & Assert
+  ASSERT_EQ(tr[0], v1);
+  ASSERT_EQ(tr[1], v2);
+  ASSERT_EQ(tr[2], v3);
+}
+
 int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
