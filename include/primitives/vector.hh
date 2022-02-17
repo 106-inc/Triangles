d341e233f147e1af376082c3cec17749b348a1d3
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-02-18 01:21:08 +0300

    [docs] First steps in docs - Added cmake file for building docs - Added documentaition for vector class
    Related to #4

--- include/primitives/vector.hh
+++ include/primitives/vector.hh
@@ -7,61 +7,241 @@
 #include <iostream>
 #include <limits>
 
+/**
+ * @file vector.hh
+ * Vector class implementation
+ */
+
 namespace geom
 {
 
+/**
+ * @concept Number
+ * @brief Useful concept which represents floating point and integral types
+ *
+ * @tparam T
+ */
 template <class T>
 concept Number = std::is_floating_point_v<T> || std::is_integral_v<T>;
 
+/**
+ * @class Vector
+ * @brief Vector class realization
+ *
+ * @tparam T - floating point type of coordinates
+ */
 template <std::floating_point T>
 struct Vector final
 {
 private:
+  /**
+   * @brief Threshold static variable for numbers comparision
+   */
   static inline T threshold_ = 1e3 * std::numeric_limits<T>::epsilon();
 
 public:
+  /**
+   * @brief Vector coordinates
+   */
   T x{}, y{}, z{};
 
+  /**
+   * @brief Construct a new Vector object from 3 coordinates
+   *
+   * @param[in] coordX x coordinate
+   * @param[in] coordY y coordinate
+   * @param[in] coordZ z coordinate
+   */
   Vector(T coordX, T coordY, T coordZ) : x(coordX), y(coordY), z(coordZ)
   {
   }
 
+  /**
+   * @brief Construct a new Vector object with equals coordinates
+   *
+   * @param[in] coordX coordinate (default to {})
+   */
   explicit Vector(T coordX = {}) : Vector(coordX, coordX, coordX)
   {
   }
 
+  /**
+   * @brief Overloaded += operator
+   * Increments vector coordinates by corresponding coordinates of vec
+   * @param[in] vec vector to incremented with
+   * @return Vector& reference to current instance
+   */
   Vector &operator+=(const Vector &vec);
+
+  /**
+   * @brief Overloaded -= operator
+   * Decrements vector coordinates by corresponding coordinates of vec
+   * @param[in] vec vector to decremented with
+   * @return Vector& reference to current instance
+   */
   Vector &operator-=(const Vector &vec);
+
+  /**
+   * @brief Unary - operator
+   *
+   * @return Vector negated Vector instance
+   */
   Vector operator-() const;
 
+  /**
+   * @brief Overloaded *= by number operator
+   *
+   * @tparam nType numeric type of value to multiply by
+   * @param[in] val value to multiply by
+   * @return Vector& reference to vector instance
+   */
   template <Number nType>
   Vector &operator*=(nType val);
 
+  /**
+   * @brief Overloaded /= by number operator
+   *
+   * @tparam nType numeric type of value to divide by
+   * @param[in] val value to divide by
+   * @return Vector& reference to vector instance
+   *
+   * @warning Does not check if val equals 0
+   */
   template <Number nType>
   Vector &operator/=(nType val);
 
+  /**
+   * @brief Dot product function
+   *
+   * @param rhs vector to dot product with
+   * @return T dot product of two vectors
+   */
   T dot(const Vector &rhs) const;
+
+  /**
+   * @brief Cross product function
+   *
+   * @param rhs vector to cross product with
+   * @return Vector cross product of two vectors
+   */
   Vector cross(const Vector &rhs) const;
 
+  /**
+   * @brief Calculate squared length of a vector function
+   *
+   * @return T length^2
+   */
   T length2() const;
+
+  /**
+   * @brief Calculate length of a vector function
+   *
+   * @return T length
+   */
   T length() const;
 
+  /**
+   * @brief Get normalized vector function
+   *
+   * @return Vector normalized vector
+   */
   Vector normalized() const;
+
+  /**
+   * @brief Normalize vector function
+   *
+   * @return Vector& reference to instance
+   */
   Vector &normalize();
 
+  /**
+   * @brief Overloaded operator [] (non-const version)
+   * To get access to coordinates
+   * @param i index of coordinate (0 - x, 1 - y, 2 - z)
+   * @return T& reference to coordinate value
+   *
+   * @note Coordinates calculated by mod 3
+   */
   T &operator[](size_t i);
+
+  /**
+   * @brief Overloaded operator [] (const version)
+   * To get access to coordinates
+   * @param i index of coordinate (0 - x, 1 - y, 2 - z)
+   * @return T coordinate value
+   *
+   * @note Coordinates calculated by mod 3
+   */
   T operator[](size_t i) const;
 
+  /**
+   * @brief Check if vector is parallel to another
+   *
+   * @param[in] rhs vector to check parallelism with
+   * @return true if vector is parallel
+   * @return false otherwise
+   */
   bool isPar(const Vector &rhs) const;
+
+  /**
+   * @brief Check if vector is perpendicular to another
+   *
+   * @param[in] rhs vector to check perpendicularity with
+   * @return true if vector is perpendicular
+   * @return false otherwise
+   */
   bool isPerp(const Vector &rhs) const;
+
+  /**
+   * @brief Check if vector is equal to another
+   *
+   * @param[in] rhs vector to check equality with
+   * @return true if vector is equal
+   * @return false otherwise
+   *
+   * @note Equality check performs using isNumEq(T lhs, T rhs) function
+   */
   bool isEqual(const Vector &rhs) const;
+
+  /**
+   * @brief Check equality (with threshold) of two floating point numbers function
+   *
+   * @param[in] lhs first number
+   * @param[in] rhs second number
+   * @return true if numbers equals with threshold (|lhs - rhs| < threshold)
+   * @return false otherwise
+   *
+   * @note Threshold defined by threshold_ static member
+   */
   static bool isNumEq(T lhs, T rhs);
 
+  /**
+   * @brief Set new threshold value
+   *
+   * @param[in] thres value to set
+   */
   static void setThreshold(T thres);
+
+  /**
+   * @brief Get current threshold value
+   */
   static void getThreshold();
+
+  /**
+   * @brief Set threshold to default value
+   * @note default value equals float point epsilon
+   */
   static void setDefThreshold();
 };
 
+/**
+ * @brief Overloaded + operator
+ *
+ * @tparam T vector template parameter
+ * @param[in] lhs first vector
+ * @param[in] rhs second vector
+ * @return Vector<T> sum of two vectors
+ */
 template <std::floating_point T>
 Vector<T> operator+(const Vector<T> &lhs, const Vector<T> &rhs)
 {
@@ -70,6 +250,14 @@ Vector<T> operator+(const Vector<T> &lhs, const Vector<T> &rhs)
   return res;
 }
 
+/**
+ * @brief Overloaded - operator
+ *
+ * @tparam T vector template parameter
+ * @param[in] lhs first vector
+ * @param[in] rhs second vector
+ * @return Vector<T> res of two vectors
+ */
 template <std::floating_point T>
 Vector<T> operator-(const Vector<T> &lhs, const Vector<T> &rhs)
 {
@@ -78,6 +266,15 @@ Vector<T> operator-(const Vector<T> &lhs, const Vector<T> &rhs)
   return res;
 }
 
+/**
+ * @brief Overloaded multiple by value operator
+ *
+ * @tparam nT type of value to multiply by
+ * @tparam T vector template parameter
+ * @param[in] val value to multiply by
+ * @param[in] rhs vector to multiply by value
+ * @return Vector<T> result vector
+ */
 template <Number nT, std::floating_point T>
 Vector<T> operator*(const nT &val, const Vector<T> &rhs)
 {
@@ -86,6 +283,15 @@ Vector<T> operator*(const nT &val, const Vector<T> &rhs)
   return res;
 }
 
+/**
+ * @brief Overloaded multiple by value operator
+ *
+ * @tparam nT type of value to multiply by
+ * @tparam T vector template parameter
+ * @param[in] val value to multiply by
+ * @param[in] rhs vector to multiply by value
+ * @return Vector<T> result vector
+ */
 template <Number nT, std::floating_point T>
 Vector<T> operator*(const Vector<T> &lhs, const nT &val)
 {
@@ -94,6 +300,15 @@ Vector<T> operator*(const Vector<T> &lhs, const nT &val)
   return res;
 }
 
+/**
+ * @brief Overloaded divide by value operator
+ *
+ * @tparam nT type of value to divide by
+ * @tparam T vector template parameter
+ * @param[in] val value to divide by
+ * @param[in] rhs vector to divide by value
+ * @return Vector<T> result vector
+ */
 template <Number nT, std::floating_point T>
 Vector<T> operator/(const Vector<T> &lhs, const nT &val)
 {
@@ -102,30 +317,72 @@ Vector<T> operator/(const Vector<T> &lhs, const nT &val)
   return res;
 }
 
+/**
+ * @brief Dot product operator
+ *
+ * @tparam T vector template parameter
+ * @param[in] lhs first vector
+ * @param[in] rhs second vector
+ * @return T dot production
+ */
 template <std::floating_point T>
 T operator&(const Vector<T> &lhs, const Vector<T> &rhs)
 {
   return lhs.dot(rhs);
 }
 
+/**
+ * @brief Cross product operator
+ *
+ * @tparam T vector template parameter
+ * @param[in] lhs first vector
+ * @param[in] rhs second vector
+ * @return T cross production
+ */
 template <std::floating_point T>
 Vector<T> operator%(const Vector<T> &lhs, const Vector<T> &rhs)
 {
   return lhs.cross(rhs);
 }
 
+/**
+ * @brief Vector equality operator
+ *
+ * @tparam T vector template parameter
+ * @param[in] lhs first vector
+ * @param[in] rhs second vector
+ * @return true if vectors are equal
+ * @return false otherwise
+ */
 template <std::floating_point T>
 bool operator==(const Vector<T> &lhs, const Vector<T> &rhs)
 {
   return lhs.isEqual(rhs);
 }
 
+/**
+ * @brief Vector inequality operator
+ *
+ * @tparam T vector template parameter
+ * @param[in] lhs first vector
+ * @param[in] rhs second vector
+ * @return true if vectors are not equal
+ * @return false otherwise
+ */
 template <std::floating_point T>
 bool operator!=(const Vector<T> &lhs, const Vector<T> &rhs)
 {
   return !(lhs == rhs);
 }
 
+/**
+ * @brief Vector print operator
+ *
+ * @tparam T vector template parameter
+ * @param[in, out] ost output stream
+ * @param[in] vec vector to print
+ * @return std::ostream& modified stream instance
+ */
 template <std::floating_point T>
 std::ostream &operator<<(std::ostream &ost, const Vector<T> &vec)
 {
