ced14032d67a7953beeba52a5394c8342f7c9dd2
Tako-San <dxe13@mail.ru>
2022-02-18 23:39:00 +0300

    Line class documentend

--- include/primitives/line.hh
+++ include/primitives/line.hh
@@ -3,28 +3,93 @@
 
 #include "vector.hh"
 
+/**
+ * @brief line.hh
+ * Line class implementation
+ */
+
 namespace geom
 {
 
+/**
+ * @class Line
+ * @brief Line class implementation
+ *
+ * @tparam T - floating point type of coordinates
+ */
 template <std::floating_point T>
 class Line final
 {
 private:
-  Vector<T> org_{}; // origin
-  Vector<T> dir_{}; // direction
+  /**
+   * @brief Origin vector
+   */
+  Vector<T> org_{};
+
+  /**
+   * @brief Direction vector
+   */
+  Vector<T> dir_{};
 
 public:
+  /**
+   * @brief Construct a new Line object
+   *
+   * @param[in] org origin vector
+   * @param[in] dir direction vector
+   */
   Line(const Vector<T> &org, const Vector<T> &dir);
 
+  /**
+   * @brief Getter for origin vector
+   *
+   * @return const Vector<T>& const reference to origin vector
+   */
   const Vector<T> &org() const;
+
+  /**
+   * @brief Getter for direction vector
+   *
+   * @return const Vector<T>& const reference to direction vector
+   */
   const Vector<T> &dir() const;
 
+  /**
+   * @brief Checks is point belongs to line
+   *
+   * @param[in] point const reference to point vector
+   * @return true if point belongs to line
+   * @return false if point doesn't belong to line
+   */
   bool belongs(const Vector<T> &point) const;
+
+  /**
+   * @brief Checks is *this equals to line
+   *
+   * @param[in] line const reference to another line
+   * @return true if lines are equal
+   * @return false if lines are not equal
+   */
   bool isEqual(const Line &line) const;
 
+  /**
+   * @brief Get line by 2 points
+   *
+   * @param[in] p1 const reference to 1st line
+   * @param[in] p2 const reference to 2nd line
+   * @return Line passing through two points
+   */
   static Line getBy2Points(const Vector<T> &p1, const Vector<T> &p2);
 };
 
+/**
+ * @brief Line print operator
+ *
+ * @tparam T - floating point type of coordinates
+ * @param[in, out] ost output stream
+ * @param[in] line Line to print
+ * @return std::ostream& modified ostream instance
+ */
 template <std::floating_point T>
 std::ostream &operator<<(std::ostream &ost, const Line<T> &line)
 {
@@ -32,6 +97,15 @@ std::ostream &operator<<(std::ostream &ost, const Line<T> &line)
   return ost;
 }
 
+/**
+ * @brief Line equality operator
+ *
+ * @tparam T - floating point type of coordinates
+ * @param[in] lhs 1st line
+ * @param[in] rhs 2nd line
+ * @return true if lines are equal
+ * @return false if lines are not equal
+ */
 template <std::floating_point T>
 bool operator==(const Line<T> &lhs, const Line<T> &rhs)
 {
