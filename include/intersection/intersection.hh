698e6f670b18fcfc6280ab96bd89de0221bb78bf
Tako-San <dxe13@mail.ru>
2022-02-27 11:23:54 +0300

    Minor

--- include/intersection/intersection.hh
+++ include/intersection/intersection.hh
@@ -24,6 +24,29 @@ bool isIntersect2D(const Triangle<T> &tr1, const Triangle<T> &tr2);
 
 /**
  * @brief Intersect 2 planes and return result of intersection
+ * @details
+ * Common intersection case (parallel planes case is trivial):
+ *
+ * Let P = point in space
+ *
+ * pl1 equation: dot(n1, P) = d1
+ * pl2 equation: dot(n2, P) = d2
+ *
+ * Intersection line direction: dir = cross(n1, n2)
+ *
+ * Let origin of intersection line be a linear combination of n1 and n2:
+ * P = a * n1 + b * n2
+ *
+ * P must satisfy both pl1 and pl2 equations:
+ * dot(n1, P) = d1 <=> dot(n1, a * n1 + b * n2) = d1 <=> a + b * dot(n1, n2) = d1
+ * dot(n2, P) = d2 <=> dot(n2, a * n1 + b * n2) = d2 <=> a * dot(n1, n2) + b = d2
+ *
+ * Let's find a and b:
+ * a = (d2 * dot(n1, n2) - d1) / ((dot(n1, n2))^2 - 1)
+ * b = (d1 * dot(n1, n2) - d2) / ((dot(n1, n2))^2 - 1)
+ *
+ * Intersection line equation:
+ * L = P + t * cross(n1, n2) = (a * n1 + b * n2) + t * cross(n1, n2)
  *
  * @tparam T - floating point type of coordinates
  * @param pl1 first plane
