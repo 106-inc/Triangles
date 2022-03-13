9d947ca22181db5c8e8bbe07394dfeecb3e9c522
Tako-San <dxe13@mail.ru>
2022-03-13 12:36:41 +0300

    Added e2e testing environment

---
+++ test/e2e/lit.cfg.py
@@ -0,0 +1,12 @@
+from os import path
+import lit.formats
+
+config.name = 'Triangles testing'
+config.test_format = lit.formats.ShTest(True)
+
+config.suffixes = ['.tst']
+
+config.test_source_root = path.dirname(__file__)
+config.test_exec_root = path.join(config.my_obj_root, 'bin/test')
+
+config.substitutions.append(('%lvl1', path.join(config.my_obj_root, 'bin/lvl1q')))
