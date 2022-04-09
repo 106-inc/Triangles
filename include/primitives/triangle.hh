6c8d03ab8ccdec9d0a591b59a9b2770675a7926e
Tako-San <dxe13@mail.ru>
2022-04-09 17:28:14 +0300

    Minor fixes in Triangles

--- include/primitives/triangle.hh
+++ include/primitives/triangle.hh
@@ -91,7 +91,7 @@ public:
    * @return true if Triangle belongs to BoundBox
    * @return false if Triangle doesn't belong to BoundBox
    */
-  bool belongsTo(const BoundBox<T> & bb) const;
+  bool belongsTo(const BoundBox<T> &bb) const;
 };
 
 /**
@@ -171,7 +171,7 @@ BoundBox<T> Triangle<T>::boundBox() const
 }
 
 template <std::floating_point T>
-bool belongsTo(const BoundBox<T> & bb) const
+bool Triangle<T>::belongsTo(const BoundBox<T> &bb) const
 {
   return boundBox().belongsTo(bb);
 }
