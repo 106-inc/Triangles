a99518dc54dd0b246befe284526aa75adccbd894
Tako-San <dxe13@mail.ru>
2022-06-02 23:23:35 +0300

    Implemented swapchain chapter

--- tools/lvl2/main.cc
+++ tools/lvl2/main.cc
@@ -3,8 +3,10 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
+#include <limits>
 #include <optional>
 #include <set>
 #include <stdexcept>
@@ -59,6 +61,13 @@ struct QueueFamilyIndices
   }
 };
 
+struct SwapChainSupportDetails
+{
+  vk::SurfaceCapabilitiesKHR capabilities;
+  std::vector<vk::SurfaceFormatKHR> formats;
+  std::vector<vk::PresentModeKHR> presentModes;
+};
+
 class HelloTriangleApplication
 {
 public:
@@ -76,13 +85,18 @@ private:
   vk::UniqueInstance instance_;
   vk::DebugUtilsMessengerEXT debugMessenger_;
   vk::DispatchLoaderDynamic dld_;
-  vk::SurfaceKHR surface_;
+  vk::SurfaceKHR surface_; // TODO: Figure out how to use unique version
 
   vk::PhysicalDevice physicalDevice_;
   vk::UniqueDevice device_;
 
   vk::Queue graphicsQueue_;
   vk::Queue presentQueue_;
+
+  vk::SwapchainKHR swapChain_; // Not unique because it has to be destroyed before surface
+  std::vector<vk::Image> swapChainImages_;
+  vk::Format swapChainImageFormat_;
+  vk::Extent2D swapChainExtent_;
 
   void initWindow()
   {
@@ -101,6 +115,7 @@ private:
     createSurface();
     pickPhysicalDevice();
     createLogicalDevice();
+    createSwapChain();
   }
 
   void mainLoop()
@@ -113,10 +128,11 @@ private:
 
   void cleanup()
   {
+    device_->destroySwapchainKHR(swapChain_);
+    instance_->destroySurfaceKHR(surface_);
+
     if constexpr (enableValidationLayers)
       instance_->destroyDebugUtilsMessengerEXT(debugMessenger_, nullptr, dld_);
-
-    instance_->destroySurfaceKHR(surface_);
 
     glfwDestroyWindow(window_);
     glfwTerminate();
@@ -147,16 +163,15 @@ private:
       createInfo.ppEnabledLayerNames = VALIDATION_LAYERS.data();
 
       populateDebugMessengerCreateInfo(debugCreateInfo);
-      vk::StructureChain<vk::InstanceCreateInfo, vk::DebugUtilsMessengerCreateInfoEXT>
-        structureChain = {createInfo, debugCreateInfo};
+      vk::StructureChain structureChain{createInfo, debugCreateInfo};
     }
 
     instance_ = vk::createInstanceUnique(createInfo);
     dld_ = {*instance_, vkGetInstanceProcAddr};
 
-    // std::cout << "available extensions:\n";
-    // for (const auto &extension : vk::enumerateInstanceExtensionProperties())
-    //   std::cout << '\t' << extension.extensionName << std::endl;
+    std::cout << "available extensions:\n";
+    for (const auto &extension : vk::enumerateInstanceExtensionProperties())
+      std::cout << '\t' << extension.extensionName << std::endl;
   }
 
   void populateDebugMessengerCreateInfo(vk::DebugUtilsMessengerCreateInfoEXT &createInfo)
@@ -240,6 +255,90 @@ private:
     presentQueue_ = device_->getQueue(indices.presentFamily.value(), 0);
   }
 
+  vk::SurfaceFormatKHR chooseSwapSurfaceFormat(
+    const std::vector<vk::SurfaceFormatKHR> &availableFormats)
+  {
+    for (const auto &availableFormat : availableFormats)
+      if (vk::Format::eB8G8R8A8Srgb == availableFormat.format &&
+          vk::ColorSpaceKHR::eSrgbNonlinear == availableFormat.colorSpace)
+        return availableFormat;
+
+    return availableFormats.front();
+  }
+
+  void createSwapChain()
+  {
+    auto swapChainSupport = querySwapChainSupport(physicalDevice_);
+
+    auto surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+    auto presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+    auto extent = chooseSwapExtent(swapChainSupport.capabilities);
+
+    auto imageCount = swapChainSupport.capabilities.minImageCount + 1;
+    if (swapChainSupport.capabilities.maxImageCount > 0 &&
+        swapChainSupport.capabilities.maxImageCount < imageCount)
+      imageCount = swapChainSupport.capabilities.maxImageCount;
+
+    vk::SwapchainCreateInfoKHR createInfo{.surface = surface_,
+                                          .minImageCount = imageCount,
+                                          .imageFormat = surfaceFormat.format,
+                                          .imageColorSpace = surfaceFormat.colorSpace,
+                                          .imageExtent = extent,
+                                          .imageArrayLayers = 1,
+                                          .imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
+                                          .preTransform = swapChainSupport.capabilities.currentTransform,
+                                          .compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
+                                          .presentMode = presentMode,
+                                          .clipped = VK_TRUE};
+
+    auto indices = findQueueFamilies(physicalDevice_);
+    std::array<uint32_t, 2> queueFamilyIndices{indices.graphicsFamily.value(),
+                                               indices.presentFamily.value()};
+
+    if (indices.graphicsFamily == indices.presentFamily)
+      createInfo.setImageSharingMode(vk::SharingMode::eExclusive);
+    else
+    {
+      createInfo.setImageSharingMode(vk::SharingMode::eConcurrent);
+      createInfo.setQueueFamilyIndices(queueFamilyIndices);
+    }
+
+    swapChain_ = device_->createSwapchainKHR(createInfo);
+    swapChainImages_ = device_->getSwapchainImagesKHR(swapChain_);
+    swapChainImageFormat_ = surfaceFormat.format;
+    swapChainExtent_ = extent;
+  }
+
+  vk::PresentModeKHR chooseSwapPresentMode(
+    const std::vector<vk::PresentModeKHR> &availablePresentModes)
+  {
+    for (const vk::PresentModeKHR &availablePresentMode : availablePresentModes)
+      if (vk::PresentModeKHR::eMailbox == availablePresentMode)
+        return availablePresentMode;
+
+    return vk::PresentModeKHR::eFifo;
+  }
+
+  vk::Extent2D chooseSwapExtent(const vk::SurfaceCapabilitiesKHR &capabilities)
+  {
+    if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+      return capabilities.currentExtent;
+
+    int width = 0;
+    int height = 0;
+    glfwGetFramebufferSize(window_, &width, &height);
+
+    vk::Extent2D actualExtent{.width = static_cast<uint32_t>(width),
+                              .height = static_cast<uint32_t>(height)};
+
+    actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width,
+                                    capabilities.maxImageExtent.width);
+    actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height,
+                                     capabilities.maxImageExtent.height);
+
+    return actualExtent;
+  }
+
   QueueFamilyIndices findQueueFamilies(vk::PhysicalDevice device)
   {
     int i = 0;
@@ -262,10 +361,29 @@ private:
     return indices;
   }
 
+  SwapChainSupportDetails querySwapChainSupport(vk::PhysicalDevice device)
+  {
+    SwapChainSupportDetails details{.capabilities = device.getSurfaceCapabilitiesKHR(surface_),
+                                    .formats = device.getSurfaceFormatsKHR(surface_),
+                                    .presentModes = device.getSurfacePresentModesKHR(surface_)};
+
+    return details;
+  }
+
   bool isDeviceSuitable(vk::PhysicalDevice device)
   {
     auto indices = findQueueFamilies(device);
-    return indices.isComplete() && checkDeviceExtensionSupport(device);
+    bool extensionsSupported = checkDeviceExtensionSupport(device);
+
+    bool swapChainAdequate = false;
+    if (extensionsSupported)
+    {
+      auto swapChainSupportDetails = querySwapChainSupport(device);
+      swapChainAdequate =
+        !swapChainSupportDetails.formats.empty() && !swapChainSupportDetails.presentModes.empty();
+    }
+
+    return indices.isComplete() && extensionsSupported && swapChainAdequate;
   }
 
   bool checkDeviceExtensionSupport(vk::PhysicalDevice device)
@@ -319,7 +437,7 @@ debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
               VkDebugUtilsMessageTypeFlagsEXT messageType,
               const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData)
 {
-  std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+  std::cerr << "[validation layer] " << pCallbackData->pMessage << std::endl;
   return VK_FALSE;
 }
 
